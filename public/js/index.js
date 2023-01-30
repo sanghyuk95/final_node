ea8abe3c7dfb2dd9972ecec59290b453f921c101
burny <shskse5@gmail.com>
2023-01-30 16:33:42 +0900

    01-30 로그인기능

--- public/js/index.js
+++ public/js/index.js
@@ -7,53 +7,151 @@ window.onload = () => {
   });
 };
 
-const $start = document.querySelectorAll("img");
-const $even = document.querySelectorAll("span:nth-child(even)");
-let delay = 0;
 
-$start.forEach((item) => {
-  item.addEventListener("click", () => {
-    introText.forEach((e) => {
-      e.style.animation = null;
-      e.style.opacity = 1;
-      e.style.animation = `smoky both`;
-      e.style.animationDuration = "3s";
-    });
-    $even.forEach((e) => {
-      e.style.animation = null;
-      e.style.opacity = 1;
-      e.style.animation = `smoky-mirror both`;
-      e.style.animationDuration = "3s";
-    });
-    introText.forEach((e) => {
-      e.style.animationDelay = `${(delay += 50)}ms`;
-    });
+const $happy = document.querySelector("#happy");
+const $happyS = document.querySelector(".happyS");
+const $happyB = document.querySelector(".happyB");
+const $happyT = document.querySelector(".happyT");
+
+const $bored = document.querySelector("#bored");
+const $boredS = document.querySelector(".boredS");
+const $boredB = document.querySelector(".boredB");
+const $boredT = document.querySelector(".boredT");
+
+const $sad = document.querySelector("#sad");
+const $sadS = document.querySelector(".sadS");
+const $sadB = document.querySelector(".sadB");
+const $sadT = document.querySelector(".sadT");
+
+const $nervous = document.querySelector("#nervous");
+const $nervousS = document.querySelector(".nervousS");
+const $nervousB = document.querySelector(".nervousB");
+const $nervousT = document.querySelector(".nervousT");
+
+const $angry = document.querySelector("#angry");
+const $angryS = document.querySelector(".angryS");
+const $angryB = document.querySelector(".angryB");
+const $angryT = document.querySelector(".angryT");
+
+const $lonely = document.querySelector("#lonely");
+const $lonelyS = document.querySelector(".lonelyS");
+const $lonelyB = document.querySelector(".lonelyB");
+const $lonelyT = document.querySelector(".lonelyT");
+
+$happy.addEventListener("animationend", () => {
+  $happyB.addEventListener("mouseover", () => {
+    $happy.style.animationName = "happyHover";
+    $happy.style.animationDelay = "0s";
+    $happy.style.animationDuration = "0.5s";
+    $happy.style.animationIterationCount = "infinite";
+    $happyS.style.animationName = "shadowHappyHover";
+    $happyS.style.animationDelay = "0s";
+    $happyS.style.animationDuration = "0.5s";
+    $happyS.style.animationIterationCount = "infinite";
+    $happyT.style.display = "block";
+    $happyT.style.top = '15px';
+    $happyT.style.right = '-15px';
+  });
+  $happyB.addEventListener("mouseout", () => {
+    $happy.style.animationName = "none";
+    $happyS.style.animationName = "none";
+    $happyT.style.display = "none";
   });
 });
 
-const $happy = document.querySelector('#happy')
-const $bored = document.querySelector('#bored')
-const $nervous = document.querySelector('#nervous')
-const $angry = document.querySelector('#angry')
-const $sad = document.querySelector('#sad')
-const $lonely = document.querySelector('#lonely')
-$nervous.addEventListener('mouseover', (e) => {
-  e.target.style.animationName = "nervousHover";
-})
-$happy.addEventListener('mouseover', (e) => {
-  e.target.style.animationName = 'happyHover'
-  e.target.style.animationDelay = '0s'
-})
-$bored.addEventListener('mouseover', (e) => {
-  e.target.style.animationName = 'boredHover'
-})
-$angry.addEventListener('mouseover', (e) => {
-  e.target.style.animationName = 'angryHover'
-  e.target.style.animationDelay = "0s";
-})
-$sad.addEventListener('mouseover', (e) => {
-  e.target.style.animationName = 'sadHover'
-})
-$lonely.addEventListener('mouseover', (e) => {
-  e.target.style.animationName = 'lonelyHover'
-})
+$bored.addEventListener("animationend", () => {
+  $boredB.addEventListener("mouseover", () => {
+    $bored.style.animationName = "boredHover";
+    $bored.style.animationDuration = "2s";
+    $bored.style.animationIterationCount = "infinite";
+    $boredS.style.animationName = "shadowBoredHover";
+    $boredS.style.animationDuration = "2s";
+    $boredS.style.animationIterationCount = "infinite";
+    $boredT.style.display = "block";
+    $boredT.style.top = "35px";
+    $boredT.style.right = '-10px';
+  });
+  $boredB.addEventListener("mouseout", () => {
+    $bored.style.animationName = "none";
+    $boredS.style.animationName = "none";
+    $boredT.style.display = "none";
+  });
+});
+
+$sad.addEventListener("animationend", () => {
+  $sadB.addEventListener("mouseover", () => {
+    $sad.style.animationName = "sadHover";
+    $sad.style.animationDuration = "3s";
+    $sad.style.animationIterationCount = "infinite";
+    $sadT.style.display = "block";
+    $sadT.style.top = "35px";
+    $sadT.style.right = "-10px";
+  });
+  $sadB.addEventListener("mouseout", () => {
+    $sad.style.animationName = "none";
+    $sadS.style.animationName = "none";
+    $sadT.style.display = "none";
+  });
+});
+
+$nervous.addEventListener("animationend", () => {
+  $nervousB.addEventListener("mouseover", () => {
+    $nervous.style.animationName = "nervousHover";
+    $nervous.style.animationDuration = "0.01s";
+    $nervous.style.animationIterationCount = "infinite";
+    $nervousS.style.animationName = "shadowNervousHover";
+    $nervousS.style.animationDuration = "0.01s";
+    $nervousS.style.animationIterationCount = "infinite";
+    $nervousT.style.display = "block";
+    $nervousT.style.top = "35px";
+    $nervousT.style.right = "-10px";
+  });
+  $nervousB.addEventListener("mouseout", () => {
+    $nervous.style.animationName = "none";
+    $nervousS.style.animationName = "none";
+    $nervousT.style.display = "none";
+  });
+});
+
+$angry.addEventListener("animationend", () => {
+  $angryB.addEventListener("mouseover", () => {
+    $angry.style.animationName = "angryHover";
+    $angry.style.animationDelay = "0s";
+    $angry.style.animationDuration = "0.5s";
+    $angry.style.transformOrigin = "50% 100%";
+    $angry.style.animationIterationCount = "infinite";
+    $angryS.style.animationName = "shadowAngryHover";
+    $angryS.style.animationDelay = "0s";
+    $angryS.style.animationDuration = "0.5s";
+    $angryS.style.animationIterationCount = "infinite";
+    $angryT.style.display = "block";
+    $angryT.style.top = "35px";
+    $angryT.style.right = "-10px";
+  });
+  $angryB.addEventListener("mouseout", () => {
+    $angry.style.animationName = "none";
+    $angryS.style.animationName = "none";
+    $angryT.style.display = "none";
+  });
+});
+
+$lonely.addEventListener("animationend", () => {
+  $lonelyB.addEventListener("mouseover", () => {
+    $lonely.style.animationName = "lonelyHover";
+    $lonely.style.animationDuration = "10s";
+    $lonely.style.animationIterationCount = "infinite";
+    $lonelyS.style.animationName = "shadowLonelyHover";
+    $lonelyS.style.animationDuration = "10s";
+    $lonelyS.style.animationIterationCount = "infinite";
+    $lonelyT.style.display = "block";
+    $lonelyT.style.top = "35px";
+    $lonelyT.style.right = "-10px";
+  });
+  $lonelyB.addEventListener("mouseout", () => {
+    $lonely.style.animationName = "none";
+    $lonelyS.style.animationName = "none";
+    $lonelyT.style.display = "none";
+  });
+});
+
+
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const session = require("express-session");
+const flash = require("connect-flash");
 
 app.use(session({ secret: "비밀코드", resave: true, saveUninitialized: false }));
 app.use(passport.initialize());
@@ -10,6 +11,7 @@ app.use(passport.session());
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.static("SVG"));
+app.use(flash());
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -38,18 +40,66 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 app.get("/login", function (req, res) {
-  res.sendFile(__dirname + "/views/login.html");
+  res.render("login.ejs");
+});
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/fail",
+  }),
+  function (req, res) {
+    res.redirect("/");
+  }
+);
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "id",
+      passwordField: "pw",
+      session: true,
+      passReqToCallback: false,
+    },
+    function (입력한아이디, 입력한비번, done) {
+      connection.query(`SELECT id,password from login where id='${입력한아이디}'`, function (err, result) {
+        if (err) {
+          return done(err);
+        }
+        if (!result) {
+          return done(null, false, { message: "존재안함" });
+        }
+        if (입력한비번 === result[0].password) {
+          return done(null, result[0]);
+        } else {
+          return done(null, false, { message: "비번틀림" });
+        }
+      });
+    }
+  )
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+  //디비에서 user.id 로 유저를 찾은뒤에 유저 정보를 밑에 중괄호에 넣음 
+  connection.query(`SELECT * from login where id='${id}'`, function (err, result) {
+    done(null,result[0]);
+  })
 });
 
-app.get("/myPage", function (req, res) {
-  connection.query("SELECT * from test", function (err, result, field) {
-    if (err) {
-      console.log(err);
-    }
-    console.log("result : ", result);
-    res.render("myPage.ejs", { data: result });
-  });
+app.get("/myPage", 로그인했니, function (req, res) {
+  console.log(req.user)
+  res.render("myPage.ejs",{data:req.user});
 });
+
+function 로그인했니(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    res.send("no");
+  }
+}
 
 app.get("/board", function (req, res) {
   connection.query("SELECT * from community", function (err, result, field) {
